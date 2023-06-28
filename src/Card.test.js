9f4b8a26e60e9fce21910f8974e48ca9b1d9786e
CodeSandbox <support@codesandbox.io>
2023-06-28 21:59:04 +0000

    Initial commit

---
+++ src/Card.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import Card from "./Card";
+
+test("it renders without crashing", () => {
+  render(<Card />);
+});
+
+test("it matches snapshot", () => {
+  const { asFragment } = render(<Card />);
+  expect(asFragment()).toMatchSnapshot();
+});
---
+++ src/Carousel.test.js
@@ -0,0 +1,39 @@
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import TEST_IMAGES from "./_testCommon.js";
+
+//smoke test
+test("it renders without crashing", () => {
+  render(<Carousel />);
+});
+
+//snapshot test
+test("it matches snapshot", () => {
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("works when you click on the right arrow", function () {
+  const { container } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+});
